2855bdccc3a94c3bee1efdf3dfb65d863ff6524a
sipankaj <pankajsingh.nitb@gmail.com>
2021-05-29 13:18:35 +0530

    App Structure

---
+++ src/main/java/com/oracle/ProjectReport/ProjectReportApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.ProjectReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oracle/ProjectReport/ProjectReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.ProjectReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
